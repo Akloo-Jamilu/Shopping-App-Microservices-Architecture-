c03c7beace2722a393fcf5335178c55671d617de
Akloo-jamilu <jhasleg126@yahoo.com>
2024-01-06 00:19:05 +0100

    order service model

--- order-service/src/main/java/com/example/orderservice/model/Order.java
+++ order-service/src/main/java/com/example/orderservice/model/Order.java
@@ -1,7 +1,24 @@
 package com.example.orderservice.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
+@Table(name="t_orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private long id;
+    private String orderNumber;
+    private List<orderLineItems> orderLineItemsList;
+
 }
--- order-service/src/main/java/com/example/orderservice/model/orderLineItems.java
+++ order-service/src/main/java/com/example/orderservice/model/orderLineItems.java
@@ -1,2 +1,20 @@
-package com.example.orderservice.model;public class orderLineItems {
+package com.example.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class orderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String skuCode;
 }
