98ff84cb00ea0942235671b2af2d765bb8eaccdd
Akloo-jamilu <jhasleg126@yahoo.com>
2024-01-09 00:18:15 +0100

    saving order based on sync request

--- microservice new/order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ microservice new/order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -8,6 +8,7 @@ import com.example.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -26,7 +28,15 @@ public class OrderService {
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
         //calling inventory service and place order if product is available
-        orderRepository.save(order);
+        Boolean result = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+        if (result){
+            orderRepository.save(order);
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
