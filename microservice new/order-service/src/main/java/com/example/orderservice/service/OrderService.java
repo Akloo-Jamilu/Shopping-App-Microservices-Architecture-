2e6ca80effce58cec440340538e885645badce00
Akloo-jamilu <jhasleg126@yahoo.com>
2024-01-13 21:01:04 +0100

    enabling client side loadbalancing

--- microservice new/order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ microservice new/order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
