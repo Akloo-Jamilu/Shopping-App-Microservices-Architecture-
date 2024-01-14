b1c615b1db5e51f5e1b09e11d1545850118678fd
Akloo-jamilu <jhasleg126@yahoo.com>
2024-01-14 23:24:45 +0100

    update

--- microservice new/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ microservice new/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -34,6 +34,10 @@ public class InventoryServiceApplication {
 			inventoryRepository.save(inventory);
 			inventoryRepository.save(inventory1);
 			inventoryRepository.save(inventory2);
+
+//			inventoryRepository.save(inventory);
+//			inventoryRepository.save(inventory1);
+//			inventoryRepository.save(inventory2);
 		};
 	}
 }
