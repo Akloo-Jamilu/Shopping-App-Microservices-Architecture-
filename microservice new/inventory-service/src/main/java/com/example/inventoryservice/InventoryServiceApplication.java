26f1c409d28b69960769b519e2d7da604b12aaeb
Akloo-jamilu <jhasleg126@yahoo.com>
2024-01-17 12:07:58 +0100

    update

--- microservice new/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ microservice new/inventory-service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@CL
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
